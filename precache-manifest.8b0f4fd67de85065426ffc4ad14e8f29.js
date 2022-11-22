0636ff9d2c0cacd5bd42b7d539b0b590b2ad0108
lukanovakgh <lukanovak.dev@gmail.com>
2022-11-22 16:51:20 +0100

    initial

---
+++ precache-manifest.8b0f4fd67de85065426ffc4ad14e8f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53525ee2c183f7493b3c",
+    "url": "/static/css/main.7829fdf5.chunk.css"
+  },
+  {
+    "revision": "53525ee2c183f7493b3c",
+    "url": "/static/js/main.326bbb2c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "551503f6fab43d9e1815",
+    "url": "/static/js/2.3fdace21.chunk.js"
+  },
+  {
+    "revision": "17e7cc96b294b2cb58dfd672fcf22341",
+    "url": "/index.html"
+  }
+];
